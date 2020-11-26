eccb480f852bff517d195c68a52a22f03ba454f3
George Karametas <georgios.karametas@viooh.com>
2020-11-26 23:06:28 +0000

    feat(button): add button component

---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+// eslint-disable-next-line react/prop-types
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
+
+export default Button
---
+++ src/components/Button.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from './Button'
+import { render, fireEvent } from '@testing-library/react'
+
+describe('Button', () => {
+  it('should render text', () => {
+    const { getByText } = render(<Button text="Attack" />)
+
+    expect(getByText(/Attack/)).toBeTruthy()
+  })
+
+  it('should fire click handler', () => {
+    const onClick = jest.fn()
+
+    const { getByText } = render(<Button text="Attack" onClick={onClick} />)
+
+    fireEvent.click(getByText(/Attack/))
+
+    expect(onClick).toHaveBeenCalled()
+  })
+})
