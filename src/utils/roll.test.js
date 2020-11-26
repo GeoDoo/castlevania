45b0698d54731c6a76fd02d3a43d29f9bbd10c4f
George Karametas <georgios.karametas@viooh.com>
2020-11-26 18:26:52 +0000

    feat(roll): add roll util to help with the dice

---
+++ src/utils/roll.test.js
@@ -0,0 +1,17 @@
+import { roll } from "./roll";
+
+test("roll", () => {
+  const dice = roll();
+
+  expect(Number.isInteger(dice)).toBe(true);
+  expect(dice < 1).toBe(false);
+  expect(dice > 6).toBe(false);
+  expect(
+    dice === 1 ||
+      dice === 2 ||
+      dice === 3 ||
+      dice === 4 ||
+      dice === 5 ||
+      dice === 6
+  ).toBe(true);
+});
