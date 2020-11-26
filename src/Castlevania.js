89011a62fda3503275e3a0ac58a44c0d2a5771a7
George Karametas <georgios.karametas@viooh.com>
2020-11-26 19:09:58 +0000

    feat(castlevania): barebones game app

---
+++ src/Castlevania.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Castlevania = () => <h1>Castlevania</h1>
+
+export default Castlevania
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Castlevania from './Castlevania'
+
+ReactDOM.render(<Castlevania />, document.getElementById('root'))
