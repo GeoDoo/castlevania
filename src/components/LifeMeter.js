d4d64c27653a4c8f0d2d9d1edab4f54e5dab1480
George Karametas <georgios.karametas@viooh.com>
2020-11-26 22:20:32 +0000

    feat(life meter): make life meter reversed

--- src/components/LifeMeter.js
+++ src/components/LifeMeter.js
@@ -3,10 +3,10 @@ import React from 'react'
 import './LifeMeter.pcss'
 
 // eslint-disable-next-line react/prop-types
-const LifeMeter = ({ name }) => (
+const LifeMeter = ({ name, reversed }) => (
   <div>
-    <h4>{name}</h4>
-    <div className="health-bar">
+    <h4 className={reversed ? 'text-right' : ''}>{name}</h4>
+    <div className={reversed ? 'reversed health-bar' : 'health-bar'}>
       <div className="bar"></div>
     </div>
   </div>
